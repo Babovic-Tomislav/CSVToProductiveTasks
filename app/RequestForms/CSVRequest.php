2e6fc3a1cd89b45d81b873f2a3f7318f6b21429e
Babovic-Tomislav <tbabovic20@foi.hr>
2021-02-25 11:30:07 +0100

    Bugfixes.

--- app/RequestForms/CSVRequest.php
+++ app/RequestForms/CSVRequest.php
@@ -16,7 +16,7 @@ class CSVRequest extends FormRequest
     public function rules()
     {
         return [
-            'csvFile'    => 'required|file|mimes:csv',
+            'csvFile'    => 'required|file|mimes:csv,text',
             'taskListId' => 'required',
             'projectId'  => 'required'
         ];
